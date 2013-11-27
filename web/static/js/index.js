9315c0d7067a1fb77f8ced452d2f2cf53c48de1c
wsds <wsds888@163.com>
2013-11-27 10:39:57 +0800

    ready to design render component.

---
+++ web/static/js/index.js
@@ -0,0 +1,42 @@
+﻿var h = window.innerHeight - 52 - 3;
+var w = window.innerWidth - 0 - 2;
+var c = document.getElementById("myCanvas");
+c.setAttribute('width', w);
+c.setAttribute('height', h);
+
+
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.fillStyle = "#FF0000";
+ctx.fillRect(0, 0, 150, 75);
+
+var test1 = document.getElementById("test1");
+c.oninput = cancasInput;
+function cancasInput() {
+    console.log("input");
+}
+
+c.onclick = cancasClick;
+function cancasClick() {
+    console.log("onclick");
+}
+
+var image = new Image();
+image.src = "/static/images/map.jpg";
+
+
+window.onload = resizeWindow;
+window.onresize = resizeWindow;
+function resizeWindow() {
+    //console.log(window.innerHeight,window.innerWidth);
+    var h = window.innerHeight - 52 - 3;
+    var w = window.innerWidth - 0 - 2;
+    var c = document.getElementById("myCanvas");
+    c.setAttribute('width', w);
+    c.setAttribute('height', h);
+    ctx.fillStyle = "#FF0000";
+
+    ctx.drawImage(image, 300, 80, 800, 506);
+
+
+}
