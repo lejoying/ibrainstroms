618ff559ae3860861956fec039c57ffba0672675
wsds <wsds888@163.com>
2013-12-14 14:52:51 +0800

    The LCA(Least Common Ancestors) has passed its test.

--- web/static/js/map.js
+++ web/static/js/map.js
@@ -1,9 +1,8 @@
 ﻿var transform = {
-    x: 226,
+    x: 50,
     y: 340
 }
-
-function locateNode(node, parent, offset_level) {
+function locateNode(node, parent, offset_level) {//offset_level here is for node's children.
     var offset = offsets[offset_level];
 
     var keys = [];
@@ -12,37 +11,47 @@ function locateNode(node, parent, offset_level) {
             keys.unshift(key);
         }
     }
-    var key;
-    var childNode;
+
     if (node.properties == null) {
         node.properties = {position: {x: 0, y: 0}};
     }
     if (node.properties.position == null) {
         node.properties.position = {x: 0, y: 0};
     }
+    //    if (node.properties.level == null) {
+    //        node.properties.level = 0;
+    //    }
 
+    var key;
+    var childNode;
     node.properties.childCount = keys.length;
-    for (var i = 0; i < keys.length; i++) {
+    for (var i = 0; i < keys.length; i++) {//resolve childNodes
         key = keys[i];
         childNode = node[key];
-        if (key != "properties") {
-            if (childNode.properties == null) {
-                childNode.properties = {position: {x: 0, y: 0}};
-            }
-            context.font = offset.font;
-            var metrics = context.measureText(key);
-            childNode.properties.textWidth = metrics.width;
-            childNode.properties.boxWidth = metrics.width + offset.text_dwidth;
-            if (parent != null) {
-
-                if (childNode.properties.position == null) {
-                    childNode.properties.position = {x: 0, y: 0};
-                }
-                childNode.properties.position.x = node.properties.boxWidth + offset.box_dwidth;
-                childNode.properties.position.y = offset.box_height * (keys.length - 1) / 2 - offset.box_height * i;
-            }
-            locateNode(childNode, node, offset.next_offset_level);
+        if (key == "properties" || key == "key") {
+            continue;
         }
+        if (childNode.properties == null) {
+            childNode.properties = {position: {x: 0, y: 0}};
+        }
+        context.font = offset.font;//wrap conntent
+        var metrics = context.measureText(key);
+        childNode.properties.textWidth = metrics.width;
+        childNode.properties.boxWidth = metrics.width + offset.text_dwidth;
+
+        //            childNode.properties.parent = node;//for lca algorithm
+        //            childNode.properties.level = node.properties.level + 1;//for lca algorithm
+        //            childNode.properties.key = key;//for lca algorithm
+
+        if (parent != null) {
+
+            if (childNode.properties.position == null) {
+                childNode.properties.position = {x: 0, y: 0};
+            }
+            childNode.properties.position.x = node.properties.boxWidth + offset.box_dwidth;
+            childNode.properties.position.y = offset.box_height * (keys.length - 1) / 2 - offset.box_height * i;
+        }
+        locateNode(childNode, node, offset.next_offset_level);
     }
 }
 
@@ -52,15 +61,16 @@ function renderNode(node, offset_level, outset) {
 
     for (var key in node) {
         var childNode = node[key];
-        if (key != "properties") {
-            context.save();
-            if (childNode.properties.color) {
-                context.strokeStyle = childNode.properties.color;
-            }
-            var childOutset = drawNode(key, childNode.properties, offset, outset);
-            renderNode(childNode, offset.next_offset_level, childOutset);
-            context.restore();
+        if (key == "properties" || key == "key") {
+            continue;
         }
+        context.save();
+        if (childNode.properties.color) {
+            context.strokeStyle = childNode.properties.color;
+        }
+        var childOutset = drawNode(key, childNode.properties, offset, outset);
+        renderNode(childNode, offset.next_offset_level, childOutset);
+        context.restore();
     }
 }
 
